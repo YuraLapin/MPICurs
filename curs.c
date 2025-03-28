4a466a7298e2a4f825317467f181c2e0f1655aad
YuraLapin <88285853+YuraLapin@users.noreply.github.com>
2025-03-28 10:13:52 +0000

    Update curs.c

--- curs.c
+++ curs.c
@@ -29,25 +29,30 @@ void merge(int arr[], int left, int mid, int right)
     i = 0;
     j = 0;
     k = left;
-    while (i < n1 && j < n2) {
-        if (left_arr[i] <= right_arr[j]) {
+    while (i < n1 && j < n2)
+    {
+        if (left_arr[i] <= right_arr[j])
+        {
             arr[k] = left_arr[i];
             ++i;
         }
-        else {
+        else
+        {
             arr[k] = right_arr[j];
             ++j;
         }
         ++k;
     }
 
-    while (i < n1) {
+    while (i < n1)
+    {
         arr[k] = left_arr[i];
         ++i;
         ++k;
     }
 
-    while (j < n2) {
+    while (j < n2)
+    {
         arr[k] = right_arr[j];
         ++j;
         ++k;
@@ -56,16 +61,38 @@ void merge(int arr[], int left, int mid, int right)
 
 
 
-void merge_sort(int arr[], int left, int right)
+void merge_sort(int arr[], int left, int right, int left_send, int right_send, int elements_per_proc)
 {
-    if (left < right) {
+    if (left < right)
+    {
         int mid = left + (right - left) / 2;
 
-        merge_sort(arr, left, mid);
-        merge_sort(arr, mid + 1, right);
-
+        merge_sort(arr, left, mid, left_send, right_send, elements_per_proc);
+        merge_sort(arr, mid + 1, right, left_send, right_send, elements_per_proc);
+        
         merge(arr, left, mid, right);
+        
+        int local_arr[elements_per_proc];
+        int i, k = 0;
+        for (i = left_send; i <= right_send; ++i)
+        {
+            local_arr[k] = arr[i];
+            ++k;
+        }
+        MPI_Gather(&local_arr, elements_per_proc, MPI_INT, &arr, elements_per_proc, MPI_INT, 0, MPI_COMM_WORLD);
     }
+}
+
+
+void print_array(int arr[], int n)
+{
+      printf("\n");
+      int i;
+      for (i = 0; i < n; ++i)
+      {
+          printf("%d ", arr[i]);            
+      }
+      printf("\n");
 }
 
 
@@ -74,7 +101,7 @@ int main(int argc, char *argv[])
 {
     int i, j, k;
     int rank, size;
-    int n = 100;
+    int n = 20;
     int arr[n];
     
     srand(time(NULL));    
@@ -95,7 +122,19 @@ int main(int argc, char *argv[])
         }
         printf("\n");
         
-        MPI_Bcast(&arr, n, MPI_INT, 0, MPI_COMM_WORLD);              
+        MPI_Bcast(&arr, n, MPI_INT, 0, MPI_COMM_WORLD);
+        for (i = 0; i < elements_per_proc - 1; ++i)
+        {
+            MPI_Gather(&local_arr, elements_per_proc, MPI_INT, &sorted_arr, elements_per_proc, MPI_INT, 0, MPI_COMM_WORLD);
+        
+            for (j = elements_per_proc; j < n + elements_per_proc; ++j)
+            {
+                arr[j - elements_per_proc] = sorted_arr[j];
+            }  
+
+            print_array(arr, n);
+        }
+        
         MPI_Gather(&local_arr, elements_per_proc, MPI_INT, &sorted_arr, elements_per_proc, MPI_INT, 0, MPI_COMM_WORLD);
         
         for (i = elements_per_proc; i < n + elements_per_proc; ++i)
@@ -105,15 +144,11 @@ int main(int argc, char *argv[])
            
         for (i = 1; i <= size - 2; ++i)
         {
+            print_array(arr, n);
             merge(arr, 0, elements_per_proc * i - 1, elements_per_proc * (i + 1) - 1);
         }
         
-        printf("\n");
-        for (i = 0; i < n; ++i)
-        {
-            printf("%d ", arr[i]);            
-        }
-        printf("\n");
+        print_array(arr, n);
     }
     
     else
@@ -123,7 +158,7 @@ int main(int argc, char *argv[])
         int left = (rank - 1) * elements_per_proc;
         int right = rank * elements_per_proc - 1;
         
-        merge_sort(arr, left, right);        
+        merge_sort(arr, left, right, left, right, elements_per_proc);        
         
         for (i = left; i <= right; ++i)
         {
@@ -131,7 +166,7 @@ int main(int argc, char *argv[])
             ++k;
         }
         
-        MPI_Gather(local_arr, elements_per_proc, MPI_INT, &arr, elements_per_proc, MPI_INT, 0, MPI_COMM_WORLD);
+        MPI_Gather(&local_arr, elements_per_proc, MPI_INT, &arr, elements_per_proc, MPI_INT, 0, MPI_COMM_WORLD);
     }
     
     MPI_Finalize();
